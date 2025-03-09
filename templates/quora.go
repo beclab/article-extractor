9885f88495b65372629be3a91f64f6eea10855aa
simon <ljx1680535@163.com>
2025-03-09 21:05:27 +0800

    add quora

--- templates/quora.go
+++ templates/quora.go
@@ -7,11 +7,43 @@ import (
 func (t *Template) QuoraScrapContent(document *goquery.Document) string {
 	contents := ""
 
-	document.Find("div.puppeteer_test_answer_content']").Each(func(i int, s *goquery.Selection) {
-		parent := s.Parent()
-		var content string
-		content, _ = goquery.OuterHtml(parent)
-		contents += content
+	document.Find("div.puppeteer_test_answer_content").Each(func(i int, s *goquery.Selection) {
+		//parent := s.Parent()
+		author := ""
+		prevNode := s.Prev()
+		if prevNode.Length() > 0 {
+			prevNode.Find("div > div > div > div > div > div > div > span > span >div >div >div >div >div > a >div > span >span").Each(func(i int, authors *goquery.Selection) {
+				authorContent, e := authors.Html()
+				if e == nil && author == "" {
+					author = authorContent
+					return
+				}
+
+			})
+		}
+
+		c1 := s.Children()
+		if c1.Length() > 0 {
+			cc1 := c1.Eq(0).Children()
+			if cc1.Length() > 1 {
+				ccc1 := cc1.Eq(1).Children()
+				if ccc1.Length() == 1 {
+					child := ccc1.Eq(0)
+					content, err := child.Html()
+					if err == nil {
+						contents += "<div class='quora-item'><span class='q-author'>" + author + "</span>" + content + "</div>"
+					}
+				} else if ccc1.Length() > 1 {
+					child := ccc1.Eq(1)
+					content, err := child.Html()
+					if err == nil {
+						contents += "<div class='quora-item'><span class='q-author'>" + author + "</span>" + content + "</div>"
+					}
+
+				}
+			}
+		}
+
 	})
 	return contents
 }
