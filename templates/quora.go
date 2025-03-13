44a3d560faa2922ceeda39d54c93609364e7ec9e
simon <ljx1680535@163.com>
2025-03-13 23:47:56 +0800

    quota

--- templates/quora.go
+++ templates/quora.go
@@ -8,38 +8,40 @@ func (t *Template) QuoraScrapContent(document *goquery.Document) string {
 	contents := ""
 
 	document.Find("div.puppeteer_test_answer_content").Each(func(i int, s *goquery.Selection) {
-		//parent := s.Parent()
-		author := ""
-		prevNode := s.Prev()
-		if prevNode.Length() > 0 {
-			prevNode.Find("div > div > div > div > div > div > div > span > span >div >div >div >div >div > a >div > span >span").Each(func(i int, authors *goquery.Selection) {
-				authorContent, e := authors.Html()
-				if e == nil && author == "" {
-					author = authorContent
-					return
-				}
-
-			})
-		}
-
-		c1 := s.Children()
-		if c1.Length() > 0 {
-			cc1 := c1.Eq(0).Children()
-			if cc1.Length() > 1 {
-				ccc1 := cc1.Eq(1).Children()
-				if ccc1.Length() == 1 {
-					child := ccc1.Eq(0)
-					content, err := child.Html()
-					if err == nil {
-						contents += "<div class='quora-item'><span class='q-author'><strong>" + author + "</strong></span>" + content + "<br></div>"
-					}
-				} else if ccc1.Length() > 1 {
-					child := ccc1.Eq(1)
-					content, err := child.Html()
-					if err == nil {
-						contents += "<div class='quora-item'><span class='q-author'><strong>" + author + "</strong></span>" + content + "<br></div>"
+		parent := s.Parent()
+		if parent.Find("div.spacing_log_originally_answered_banner").Length() == 0 {
+			author := ""
+			prevNode := s.Prev()
+			if prevNode.Length() > 0 {
+				prevNode.Find("div > div > div > div > div > div > div > span > span >div >div >div >div >div > a >div > span >span").Each(func(i int, authors *goquery.Selection) {
+					authorContent, e := authors.Html()
+					if e == nil && author == "" {
+						author = authorContent
+						return
 					}
 
+				})
+			}
+
+			c1 := s.Children()
+			if c1.Length() > 0 {
+				cc1 := c1.Eq(0).Children()
+				if cc1.Length() > 1 {
+					ccc1 := cc1.Eq(1).Children()
+					if ccc1.Length() == 1 {
+						child := ccc1.Eq(0)
+						content, err := child.Html()
+						if err == nil {
+							contents += "<div class='quora-item'><span class='q-author'><strong>" + author + "</strong></span>" + content + "<br></div>"
+						}
+					} else if ccc1.Length() > 1 {
+						child := ccc1.Eq(1)
+						content, err := child.Html()
+						if err == nil {
+							contents += "<div class='quora-item'><span class='q-author'><strong>" + author + "</strong></span>" + content + "<br></div>"
+						}
+
+					}
 				}
 			}
 		}
